c56ba48ea0581143665c4bea8edda1ecb835b300
Zhu Zhaomeng <zhaomeng.zhu@gmail.com>
2022-03-17 21:59:58 +0800

    feat: add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.10-slim
+
+WORKDIR /karez
+
+ENV PYTHONPATH="${PYTHONPATH}:/opt/" \
+    PYTHONUNBUFFERED=1 \
+    PIP_DISABLE_PIP_VERSION_CHECK=on
+
+COPY pyproject.toml /karez/
+COPY karez/ /karez/karez
+RUN pip install poetry
+RUN poetry config virtualenvs.create false
+RUN poetry install --no-dev
+
+ENTRYPOINT ["karez"]
--- karez/main.py
+++ karez/main.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 from pathlib import Path
+from typing import Union
 
 import typer
 from dynaconf import Dynaconf
@@ -10,24 +11,33 @@ from karez.utils import search_plugins
 
 def launch_role(role, plugin_path, config, event_loop, nats_addr):
     role_lib = search_plugins([Path(plugin_path, role.lower())], role)
+    count = 0
     for role_config in config.get(f"{role.lower()}s", []):
         ins = role_lib.get(role_config.type)(role_config, nats_addr=nats_addr)
         event_loop.create_task(ins.run())
+        count += 1
+    logging.info(f"Launched {count} {role}s.")
 
 
-def main(config_files: list[Path],
+def main(config_files: list[Path] = typer.Option(None, "--config", "-c"),
          plugin_path: Path = typer.Option("plugins", "--plugin-directory", "-p"),
-         nats_addr: str = typer.Option("nats://localhost:4222", "--nats-addr", "-n"),
-         logging_level: str = typer.Option("WARNING", "--logging-level", "-l"),
+         nats_addr: str = typer.Option("nats://localhost:4222", "--nats-addr", "-a"),
+         logging_level: str = typer.Option("INFO", "--logging-level", "-l"),
          launch_dispatcher: bool = typer.Option(False, "--dispatcher", "-d"),
-         launch_connector: bool = typer.Option(False, "--connector", "-c"),
+         launch_connector: bool = typer.Option(False, "--connector", "-n"),
          launch_converter: bool = typer.Option(False, "--converter", "-v"),
          ):
     logging.basicConfig()
     logging.getLogger().setLevel(getattr(logging, logging_level))
 
+    logging.info(f"Configurations: {config_files or './config/*'}.")
+    logging.info(f"NATS address: {nats_addr}.")
+
+    if config_files:
+        config = Dynaconf(settings_files=config_files)
+    else:
+        config = Dynaconf(includes=["./config/*"])
     event_loop = asyncio.new_event_loop()
-    config = Dynaconf(settings_files=config_files)
     args = plugin_path, config, event_loop, nats_addr
 
     if not (launch_dispatcher or launch_connector or launch_converter):
