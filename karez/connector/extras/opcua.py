6b9347baf63af92b4eba3fd1dc2d45f08e361d3a
tefx <zhaomeng.zhu@gmail.com>
2022-05-20 14:26:55 +0800

    feat: add timeout option in opcua connector

--- karez/connector/extras/opcua.py
+++ karez/connector/extras/opcua.py
@@ -2,7 +2,7 @@ from numbers import Number
 
 from asyncua import Client
 
-from ...config import ConfigEntity
+from ...config import ConfigEntity, OptionalConfigEntity
 from ..base import PullConnectorBase
 
 
@@ -15,13 +15,14 @@ class Connector(PullConnectorBase):
     def config_entities(cls):
         yield from super(Connector, cls).config_entities()
         yield ConfigEntity("url", "OPC-UA server url.")
+        yield OptionalConfigEntity("timeout", 60, "Request timeout")
 
     @classmethod
     def role_description(cls):
         return "Connector for OPCUA."
 
     def create_client(self):
-        return Client(url=self.url)
+        return Client(url=self.url, timeout=self.config.timeout)
 
     async def fetch_data(self, client: Client, entities):
         nodes = [client.get_node(node_id) for node_id in entities]
