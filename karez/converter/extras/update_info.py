c83c392662af0df98b8536b69b15b2202d91b4fe
tefx <zhaomeng.zhu@gmail.com>
2022-06-09 20:08:40 +0800

    feat: allow remove fields in update_info converter

--- karez/converter/extras/update_info.py
+++ karez/converter/extras/update_info.py
@@ -1,6 +1,6 @@
 import json
 
-from ...config import ConfigEntity
+from ...config import ConfigEntity, OptionalConfigEntity
 from ..base import ConverterBase
 
 
@@ -14,10 +14,14 @@ class Converter(ConverterBase):
         yield from super(Converter, cls).config_entities()
         yield ConfigEntity("info_file", "Json file contains data point meta data.")
         yield ConfigEntity("key", "Key name.")
+        yield OptionalConfigEntity("remove", None, "remove fields after updating.")
 
     def convert(self, payload):
         with open(self.config.info_file, "r") as f:
             info = json.load(f)
         key = self.config.key
         payload.update(info[payload[key]])
+        if self.config.remove:
+            for key in self.config.remove.split(","):
+                del payload[key.strip()]
         yield payload
